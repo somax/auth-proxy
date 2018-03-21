cffd81bec24b8e46fdf0bf2dc713aeb88d9fd517
somax <somaxj@163.com>
2018-03-21 11:59:42 +0800

    增加 cookie 中 token 机制校验机制

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const auth = require('basic-auth');
 const httpProxy = require('http-proxy');
 const log = require('solog');
 const jf = require('jsonfile');
-var QRCode = require('qrcode');
+const QRCode = require('qrcode');
+const uuid = require('uuid/v4');
+const cookie = require('cookie');
 
 const speakeasy = require('speakeasy');
 
@@ -15,6 +17,7 @@ ks.insert('aaaaaaa',2000);
 ks.insert('bbbbbbb',2000);
 ks.insert('ccccccc',5000);
 
+log.setDevelopMode(process.env.PROXY_DEBUG === 'true');
 
 const TARGET_HOST = process.env.PROXY_TARGET_HOST;
 const TARGET_PORT = process.env.PROXY_TARGET_PORT;
@@ -29,6 +32,9 @@ const ENABLE_2FA = process.env.PROXY_ENABLE_2FA === 'true';
 const SECRET_LENGTH = process.env.PROXY_SECRET_LENGTH || 20
 const SECRET_FILE = process.env.PROXY_SECRET_FILE || '.secret';
 
+// 允许密码有效期为前后 n 秒，n = i * 30
+const _2FA_WINDOW = 10; //TODO 调试方便先设大点，回头改回 1
+const AUTH_TOKEN_COOKIE_NAME = '_APTK' ;
 
 // two factor auth
 let secret;
@@ -66,26 +72,36 @@ function startServer() {
         log.err(err)
     });
 
-
-    function deny(res) {
+    function deny(res, msg) {
         res.statusCode = 401;
         res.setHeader('WWW-Authenticate', 'Basic realm="auth-proxy"');
         res.end('Access denied');
     }
 
+    function basicAuthCheck(credentials) {
+        return !credentials || credentials.name !== NAME || credentials.pass !== PASS
+    }
+
+    function setCookie(res, name, value) {
+        res.setHeader('Set-Cookie', cookie.serialize(name, value, {
+            httpOnly: true,
+            maxAge: 60 * 60 * 24 * 7 // 1 week 
+        }));
+    }
+
     const proxyServer = http.createServer(function(req, res) {
-        var credentials = auth(req);
+        var _credentials = auth(req);
 
-
+        // 强制清理 base auth 缓存
         if (req.url === '/_auth_proxy_logout') {
-            res.statusCode = 401;
-            res.setHeader('WWW-Authenticate', 'Basic realm="auth-proxy"');
-            res.end('logout');
-        } else if (req.url === '/_auth_proxy_otp') {
-
-            // 获得二维码
+            // TODO 删除 cookie 中的 token
+            deny(res,'Logout')
+        }
+        
+        // 获得二维码，用来设置手机端 2FA 账户
+        else if (req.url === '/_auth_proxy_otp') {
             if (ENABLE_2FA) {
-                if (!credentials || credentials.name !== NAME || credentials.pass !== PASS) {
+                if (basicAuthCheck(_redentials)) {
                     deny(res)
                 } else {
                     QRCode.toDataURL(secret.otpauth_url, function(err, data_url) {
@@ -96,33 +112,62 @@ function startServer() {
             } else {
                 res.end('ENABLE_2FA=false');
             }
-        } else {
+        }
+        
+        // 剩下的就是正常代理网站内容了
+        else {
 
             if (ENABLE_2FA) {
-                if (credentials && credentials.name === NAME) {
-                    log.debug('credentials', credentials);
+
+                // TODO 
+                // - [√] 检查 cookies 中的 token 是否存在于 key - store 中，
+                // - [√] 如果存在直接放行，如果不存在则验证密码，
+                // - [√] 验证密码通过，生成 token 存入 key-store 然后加入 cookies
+                // - [ ] 检测有效 token 的剩余生命期，如果生命减半，则更新 token
+                // if(ks.check())
+                let cookies = cookie.parse(req.headers.cookie || '');
+                log.debug('------------cookies----', cookies)
+                let tokenVerified = ks.check(cookies[AUTH_TOKEN_COOKIE_NAME]);
+                log.debug('tokenVerified:',tokenVerified)
+
+                if (tokenVerified) {
+                    proxy.web(req, res);
+                    return;
+                }
+
+
+                // 开启两步验证的方式，先检查用户名
+                if (_credentials && _credentials.name === NAME) {
+                    log.debug('_credentials', _credentials);
                     log.debug('secret', secret);
 
+                    // 再验证 2FA 密码
+
+                    let userToken = _credentials.pass;
+
+                    // TODO remove when debug done >>
                     let token = speakeasy.totp({
                         secret: secret.base32,
                         encoding: 'base32'
                     });
-
-
-
-                    let userToken = credentials.pass;
                     log.debug(token, userToken);
+                    // <<
 
                     let verified = speakeasy.totp.verify({
                         secret: secret.base32,
                         encoding: 'base32',
                         token: userToken,
-                        window: 10 // 允许前后 30秒 有效
+                        window: _2FA_WINDOW
                     });
 
                     log.debug('verified', verified);
 
                     if (verified) {
+
+                        // TODO 生成 token 存入 cookie
+                        let token = ks.insert(uuid());
+                        setCookie(res, AUTH_TOKEN_COOKIE_NAME, token)
+
                         proxy.web(req, res);
                     } else {
                         deny(res);
@@ -133,19 +178,15 @@ function startServer() {
                     deny(res);
                 }
             } else {
-                if (!credentials || credentials.name !== NAME || credentials.pass !== PASS) {
+
+                // 不用两步验证的模式，就是简单的 basic auth 了
+                if (basicAuthCheck(_redentials)) {
                     deny(res);
                 } else {
                     proxy.web(req, res);
                 }
             }
-
-
-
         }
-
-
-
     });
 
     // 同时代理 Websocket
@@ -155,13 +196,13 @@ function startServer() {
 
     proxyServer.listen(PORT);
 
-    log(`Start at http://0.0.0.0:${PORT}, proxy to ${TARGET_HOST}:${TARGET_PORT}`)
+    log.info(`Start at http://0.0.0.0:${PORT}, proxy to ${TARGET_HOST}:${TARGET_PORT}`)
 
     if(ENABLE_2FA){
-        log(`To scan qrcode image at: http://0.0.0.0:${PORT}/_auth_proxy_otp`)
+        log.info(`To scan qrcode image at: http://0.0.0.0:${PORT}/_auth_proxy_otp`)
     }
 
-    log(`Browser to http://0.0.0.0:${PORT}/_auth_proxy_logout to force logout`)
+    log.info(`Browser to http://0.0.0.0:${PORT}/_auth_proxy_logout to force logout`)
 }
 
 
